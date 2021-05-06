d69870210a574880c0c18685f05c3cd02fed592d
k-kuwa987 <k-kuwa987@akane.waseda.jp>
2021-05-06 09:03:40 +0900

    made common style file of users

--- pages/users/auth-register.vue
+++ pages/users/auth-register.vue
@@ -74,50 +74,7 @@ export default {
 };
 </script>
 <style scoped>
-.register {
-  display: flex;
-  align-items: center;
-  height: 100vh;
-}
-
-.register-modal {
-  display: flex;
-  align-items: center;
-  justify-content: center;
-  flex-direction: column;
-  color: #555454;
-  background: #ffffff 0% 0% no-repeat padding-box;
-  box-shadow: 0rem 1.6rem 5.4rem #89bdbd80;
-  border-radius: 1rem;
-  opacity: 1;
-  width: 60%;
-  height: 85%;
-  margin: 0 auto;
-}
-.register-modal__title {
-  margin-bottom: 8.6rem;
-  font: normal bold 3.2rem/4.8rem Meiryo;
-  letter-spacing: 0rem;
-  font-size: 3.2rem;
-  color: #707070;
-  opacity: 1;
-}
-.register-modal__form {
-  width: 60%;
-  margin-bottom: 2.8rem;
-}
-.register-modal__input {
-  width: 100%;
-}
 .register-modal__button {
   width: 20rem;
-}
-.register-modal__label {
-  text-align: left;
-  font: normal normal normal 2rem/3rem Meiryo;
-  letter-spacing: 0rem;
-  font-size: 2rem;
-  color: #707070;
-  opacity: 1;
 }
 </style>
