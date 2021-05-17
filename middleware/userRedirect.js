3c317c7e867edc7d37859d9ccbd618c98dea9844
k-kuwa987 <k-kuwa987@akane.waseda.jp>
2021-05-18 08:51:55 +0900

    fix

--- middleware/userRedirect.js
+++ middleware/userRedirect.js
@@ -1,13 +1,18 @@
-
 // ユーザー登録関連はバックオフィス以上のみアクセス可能
 // それ以外の権限の人はトップページへリダイレクトさせる
 // オーナー：4,管理者；3,バックオフィス：2
-export default function ({ redirect, $auth }) {
-  const role = $auth.user.role
+export default function({ redirect, $auth, route }) {
+  const role = $auth.user.role;
+  const path = route.path;
   const canPathUser = [4, 3, 2];
   if (canPathUser.includes(role)) {
-    return
+    return;
   } else {
-    redirect('/');
+    //オーナーが編集画面に直接アクセスしようとした場合はエラー画面へ遷移
+    if (path.match(/.*(\/user\/).*(\/edit)/g)) {
+      redirect("/error");
+    } else {
+      redirect("/");
+    }
   }
 }
